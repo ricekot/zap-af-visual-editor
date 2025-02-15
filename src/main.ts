29469a361c522262b42603665096e2216c7c1d05
ricekot <git@ricekot.com>
2025-02-16 00:36:53 +0530

    Add tailwind, prettier, and main page structure
    Signed-off-by: ricekot <git@ricekot.com>

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import zapLogo from '../public/zap.svg';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div class="h-screen flex flex-col">
+    <nav class="h-12 bg-gray-800 text-white flex items-center px-4">
+        <img src="${zapLogo}" class="h-8 mr-2.5" alt="ZAP logo" />
+        ZAP AF Plan Visualizer
+    </nav>
+    <div class="flex flex-1">
+        <div class="w-1/2 p-4">
+            <textarea id="editor" class="w-full h-full border">
+jobs:
+  - type: script
+  - type: spider
+            </textarea>
+        </div>
+        <div class="w-1/2 p-4 overflow-auto">
+            <div id="preview" class="border p-4 bg-white">
+                <pre class="mermaid"></pre>
+            </div>
+        </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+`;
