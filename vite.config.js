683a37a2d6af3f7f685c1901820c870ba9ca2888
ricekot <git@ricekot.com>
2025-02-16 22:27:51 +0530

    Publish to GH pages
    Signed-off-by: ricekot <git@ricekot.com>

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/zap-af-visual-editor",
   plugins: [tailwindcss()],
 });
